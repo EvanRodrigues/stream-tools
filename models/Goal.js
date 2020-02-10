2c0d334d4e3a762c97e042bebb0b892065fc0a4f
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-02-10 14:09:25 -0500

    Added Goal model and updated endpoints

--- models/Goal.js
+++ models/Goal.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GoalSchema = new Schema({
+    channel: {
+        type: String,
+        required: true
+    },
+    progress: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    goal: {
+        type: Number,
+        required: true,
+        default: 100.0
+    },
+    name: {
+        type: String,
+        required: true,
+        default: "My Goal"
+    },
+    sub_val: {
+        type: Number,
+        required: true,
+        default: 5.0
+    }
+});
+
+module.exports = Goal = mongoose.model('goal', GoalSchema);
--- routes/api/goal.js
+++ routes/api/goal.js
@@ -4,15 +4,89 @@ const router = express.Router();
 const GoalController = require('../../controllers/GoalController');
 let goal = require('../../public/data/data.json');
 
+//Goal Model
+const Goal = require('../../models/Goal');
+
+//@route    GET api/goal
+//@desc     Get all goals
+//@access   Public  
 router.get('/', (req, res) => {
-    res.json(goal);
+    Goal.find()
+        .then(goals => res.json(goals));
 });
 
+
+//@route    GET api/goal/:channel
+//@desc     Get all goals
+//@access   Public
+//TODO      Could have this use twitch oauth to access the correct goal  
+router.get('/:channel', (req, res) => {
+    Goal.findOne({ channel: req.params.channel })
+        .then(goal => res.json(goal));
+});
+
+
+//@route    POST api/goal
+//@desc     Creates a new goal
+//@access   Public
+router.post('/', (req, res) => {
+    const newGoal = new Goal({
+        channel: req.body.channel,
+        progress: req.body.progress,
+        goal: req.body.goal,
+        name: req.body.name,
+        sub_val: req.body.sub_val
+    });
+
+    newGoal.save().then(item => res.json(item));
+});
+
+
+//@route    DELETE api/goal/:id
+//@desc     Deletes a goal
+//@access   Public
+router.delete('/:id', (req, res) => {
+    Goal.findById(req.params.id)
+        .then(goal => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false })); //id not found
+});
+
+
+//@route    POST api/goal
+//@desc     Updates the progress of the goal
+//@access   Public
+router.post('/updateProgress/:channel', (req, res) => {
+    Goal.findOne({ channel: req.params.channel })
+        .then(goal => {
+            goal.progress = req.body.progress;
+            goal.save()
+                .then(goal => res.json(goal));
+        });
+
+    /*let curr_goal = new GoalController(goal["progress"], goal["goal"], goal["name"], goal["sub_val"]);
+    curr_goal.updateProgress(parseFloat(req.body.progress));
+
+    fs.writeFile('./public/data/data.json', JSON.stringify(curr_goal.toJSON(), null, 2), err => {
+        if (err) console.log(err);
+    })
+
+    res.json(curr_goal.toJSON());*/
+})
+
+
+//@route    GET api/goal/accessToken
+//@desc     Gets the streamlabs access token
+//@access   Public
+//TODO      Make this require authentication
 router.get('/accessToken', (req, res) => {
     const token = process.env.STREAMLABS_API_TOKEN;
     const json_output = { "token": token };
     res.json(json_output);
 })
+
+
+
+
 
 //Resets the goal to 0.
 router.get('/reset');
@@ -29,18 +103,7 @@ router.post('/update', (req, res) => {
     res.json(new_goal.toJSON());
 });
 
-//Updates the progress of the goal to :progress
-//Saves the progress to data.json
-router.post('/updateProgress', (req, res) => {
-    let curr_goal = new GoalController(goal["progress"], goal["goal"], goal["name"], goal["sub_val"]);
-    curr_goal.updateProgress(parseFloat(req.body.progress));
 
-    fs.writeFile('./public/data/data.json', JSON.stringify(curr_goal.toJSON(), null, 2), err => {
-        if (err) console.log(err);
-    })
-
-    res.json(curr_goal.toJSON());
-})
 
 //Updates the goal's name.
 router.post('/updateName/:name', (req, res) => {
