e48160160937fdc9c713e2a248ffc942f0e87890
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-03-13 13:16:21 -0400

    Added nav, banner, and color inputs

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,3 +1,38 @@
+function formatNumber(number) {
+    return number.toFixed(2);
+}
+
+async function updateGoal() {
+    const url = window.location.origin;
+    let twitch_channel;
+    const progress = parseFloat($("#progress").val());
+    const goal_target = parseFloat($("#goal_target").val());
+    const goal_name = $("#goal_name").val();
+    const sub_val = parseFloat($("#sub_val").val());
+
+    try { //Dev
+        twitch_channel = channel;
+    }
+    catch (err) { //Live
+        twitch_channel = await getChannel(url);
+    }
+
+    const data = {
+        "progress": progress,
+        "goal": goal_target,
+        "name": goal_name,
+        "sub_val": sub_val
+    };
+
+    $.ajax({
+        type: "POST",
+        url: url + "/api/goal/update/" + twitch_channel,
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: (data) => { window.location.href = url; }
+    });
+};
 
 async function getGoal(twitch_channel, url) {
     const api_url = url + "/api/goal/channel/" + twitch_channel;
@@ -8,6 +43,11 @@ async function getGoal(twitch_channel, url) {
     $("#goal_target").val(json["goal"]);
     $("#goal_name").val(json["name"]);
     $("#sub_val").val(json["sub_val"]);
+
+
+    $("#current").html("$" + formatNumber(json["progress"]));
+    $("#goalName").html(json["name"]);
+    $("#goal").html("$" + formatNumber(json["goal"]));
 }
 
 //Gets the channel name from the backend.
@@ -35,5 +75,5 @@ $(document).ready(async function () {
 
     getGoal(twitch_channel, url);
 
-    //TODO: set text values on bar.
+    $("#barSubmit").click(updateGoal);
 });
