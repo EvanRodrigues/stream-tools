5cc7ecff49aae6a1af2123865f7eef581c7330dd
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-06-04 15:18:49 -0400

    Added frontend validation to dashboard

--- client/src/components/GoalSettings.js
+++ client/src/components/GoalSettings.js
@@ -2,23 +2,42 @@ import React, { useState, useEffect } from "react";
 import { GoalInput } from "./GoalInput";
 
 export const GoalSettings = (props) => {
-    const [inputProgress, setInputProgress] = useState(0.0);
+    const [inputProgress, setInputProgress] = useState("");
     const [inputName, setInputName] = useState("");
     const [inputGoal, setInputGoal] = useState("");
 
     const updateProgress = (event) => {
+        validateDollars(event.target.value);
         setInputProgress(event.target.value);
         props.setProgress(parseFloat(event.target.value));
     };
 
     const updateGoal = (event) => {
+        validateDollars(event.target.value);
         setInputGoal(event.target.value);
         props.setGoal(parseFloat(event.target.value));
     };
 
     const updateName = (event) => {
+        validateName(event.target.value);
         setInputName(event.target.value);
         props.setName(event.target.value);
+    };
+
+    const validateName = (nameInput) => {
+        if (nameInput.length == 0)
+            props.setError("Goal Name can not be empty!");
+        else props.setError("");
+    };
+
+    const validateDollars = (dollarInput) => {
+        const dollarRegex = /^\d+\.\d\d$/;
+
+        if (dollarRegex.test(dollarInput) == false) {
+            props.setError("Progress and Goal Target not formatted correctly!");
+        } else {
+            props.setError("");
+        }
     };
 
     useEffect(() => {
@@ -54,6 +73,10 @@ export const GoalSettings = (props) => {
             />
             <GoalInput label="Font (not used)" value="" name="font" />
             <GoalInput label="Font Size (not used)" value="" name="fontSize" />
+
+            <span id="goalError" className="errorMessage">
+                {props.error}
+            </span>
         </div>
     );
 };
