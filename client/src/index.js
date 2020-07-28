77c3acc4267c0115b3cdecc639faf8570c4468ad
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-07-28 17:29:43 -0400

    Added initial redux boiler plate

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Nav } from "../components/Nav";
 import "../stylesheets/css/nav.css";
 import { CLIENT_ID } from "../config/keys";
+import { logout } from "../actions/isLogged";
+import { setUser } from "../actions/user";
 
 export const Home = () => {
+    const dispatch = useDispatch();
     const redirect_uri = `${window.location.origin}/LogIn`;
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [user, setUser] = useState("");
+    const isLogged = useSelector((state) => state.isLogged);
+    const user = useSelector((state) => state.user);
+
     const [url, setUrl] = useState(
         `https://id.twitch.tv/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${redirect_uri}&response_type=code&scope=user:read:email`
     );
 
-    const LogOut = () => {
-        setLoggedIn(false);
-    };
-
-    if (loggedIn === false) {
+    if (isLogged === false) {
         return (
             <div>
                 <h1>YOU NEED TO LOG IN!</h1>
+                <button onClick={() => dispatch(setUser("doopian"))}>+</button>
+                <h2>{user}</h2>
                 <a href={url}>Log In</a>
             </div>
         );
@@ -26,8 +29,8 @@ export const Home = () => {
     return (
         <>
             <Nav />
-            <h2>{user}</h2>
-            <a onClick={LogOut}>Log Out</a>
+            <h2>LOGGED IN AS: {user}</h2>
+            <a onClick={dispatch(logout)}>Log Out</a>
         </>
     );
 };
